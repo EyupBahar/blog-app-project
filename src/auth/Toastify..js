4f8cfe380d96be7dc0cc4aecfa78ef436b171291
7204-OMER <Eyupbhr24@gmail.com>
2021-06-27 11:33:19 +0300

    add Toastify.js

--- src/auth/Toastify..js
+++ src/auth/Toastify..js
@@ -10,4 +10,24 @@ export const toastWarnNotify = (msg) => {
     progress: undefined,
   });
 };
-
+export const toastSuccessNotify = (msg) => {
+    toast.success(msg, {
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  
+  export const toastErrorNotify = (msg) => {
+    toast.error(msg, {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
