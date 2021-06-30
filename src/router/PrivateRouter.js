bf0543b4acb61a0f20b2f360bbf0b6b5468fc860
7204-OMER <Eyupbhr24@gmail.com>
2021-06-30 13:50:55 +0300

    add private router

---
+++ src/router/PrivateRouter.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (currentUser?.displayName) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
