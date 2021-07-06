b9e9296514db74446fdd0bcf4fc76d9e7837c62c
7204-OMER <Eyupbhr24@gmail.com>
2021-07-06 12:31:30 +0300

    add dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,9 @@
 import React from "react";
-import PostCard from "../components/PostCard";
 import PostContainer from "../components/PostContainer";
 
 const Dashboard = () => {
   return (
     <div>
-      {/* <PostCard /> */}
       <PostContainer />
     </div>
   );
