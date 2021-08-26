180b07b3f4e31855db8aaa0a717605cf537dddbe
7204-OMER <Eyupbhr24@gmail.com>
2021-08-26 22:13:51 +0300

    last commit

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -112,7 +112,6 @@ export const useFetch = () => {
         blogArray.push({ id, ...blogs[id] });
       }
       setBlogList(blogArray);
-      console.log("before setloading is false")
       setIsLoading(false);
       
     });
